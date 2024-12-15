35220ea7595baa3ab165a01b2ba6e4d8b7bc128e
Avaz <avazbekrozimboyev08@gmail.com>
2024-12-15 07:33:15 +0500

    sajcjsc

---
+++ main.js
@@ -0,0 +1,11 @@
+
+
+class Book{
+    constructor(title,author,genre,available){
+        this.title=title;
+        this.author=author;
+        this.genre=genre;
+        this.available=available;
+    }
+}
+const book1=new Book()
